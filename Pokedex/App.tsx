cb8e5406183c56f560c1c7dd670199f6cd7860b6
Leo Silva <devleonardosilva@gmail.com>
2022-03-16 13:23:42 -0300

    first commit

---
+++ Pokedex/App.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+ Text,
+ View,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+ 
+} from 'react-native/Libraries/NewAppScreen';
+
+// const Section: React.FC<{
+//   title: string;
+// }> = ({children, title}) => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <View>
+   <Text>hvjkvhv</Text> 
+   </View>
+  );
+};
+
+
+export default App;
