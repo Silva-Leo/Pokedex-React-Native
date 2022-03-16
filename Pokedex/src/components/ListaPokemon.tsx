7b9262f57c72a2268dc8c16bba06613667f12b25
Leo Silva <devleonardosilva@gmail.com>
2022-03-16 20:00:45 -0300

    FEAT: primeiro get para API do pokemon

--- Pokedex/src/components/ListaPokemon.tsx
+++ Pokedex/src/components/ListaPokemon.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 import { Text, View } from 'react-native';
 
 import { PokemonClient }from 'pokenode-ts';
 
 const ListaPokemon = ()=>{
+    useEffect(()=>{
+        const fetchPokemon = async ()=>{
+            const api = new PokemonClient();
+            await api.getPokemonById(4)
+            .then(pokemon =>{
+                console.log(pokemon);
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+            // await api.getPokemonSpeciesByName
+            // tamanho / habilidade / genero 
+        };
+        fetchPokemon();
+    })
+
+
     <View>
         <Text>
             LIsta aqui
