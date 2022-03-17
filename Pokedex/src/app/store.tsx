5692a9bd363af59827899995033a76ce962aca3b
Leo Silva <devleonardosilva@gmail.com>
2022-03-17 20:08:22 -0300

    REFACT: Store organizado

--- Pokedex/src/app/store.tsx
+++ Pokedex/src/app/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import pokemonReducer from "../features/numeroPokemon/numeroDoPokemon";
+import counterReducer from '../features/numeroPokemon/numeroDoPokemon';
+import pokemonReducer from "../features/pokemon/pokemonSlice";
 
 export const store = configureStore({
     reducer:{
+        counter: counterReducer,
         pokemon: pokemonReducer,
     }
 });
