c77ec76e0f7584e3d601db2b645aa54099df0b80
Leo Silva <devleonardosilva@gmail.com>
2022-03-17 20:08:38 -0300

    CHORE: Limpando hooks

--- Pokedex/src/app/hooks.tsx
+++ Pokedex/src/app/hooks.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable eol-last */
-/* eslint-disable prettier/prettier */
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from './store';
 
