7fe8f6c59f3e9d1fc82eaad0887b9c3b49d8fe41
Leo Silva <devleonardosilva@gmail.com>
2022-03-17 18:49:24 -0300

    FEAT: criacao hooks.tsx

---
+++ Pokedex/src/app/hooks.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable eol-last */
+/* eslint-disable prettier/prettier */
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { RootState, AppDispatch } from './store';
+
+// aqui está exportando duas variáveis que têm os tipos de digitação certos definidos.
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
