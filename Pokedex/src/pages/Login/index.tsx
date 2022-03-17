cadd9b452736ce311751a9aedfd57bd70af27fa3
Leo Silva <devleonardosilva@gmail.com>
2022-03-17 20:22:41 -0300

    STYLE: page login criada

--- Pokedex/src/pages/Login/index.tsx
+++ Pokedex/src/pages/Login/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { View, Text } from 'react-native';
 
 const Login = ()=>{
     return(
-        <>
-        </>
+        <View style={{flexDirection:'column', alignItems:'center', justifyContent:'center',flex: 1}}>
+            <Text style={{fontSize: 20}}>Login</Text>
+        </View>
     )
 }
 
