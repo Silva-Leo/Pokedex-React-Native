80b198d9dbb566f601d6ba41700f88122c5ea321
Leo Silva <devleonardosilva@gmail.com>
2022-03-17 18:47:05 -0300

    FEAT: informacoes do pokemon

--- Pokedex/src/features/pokemon/pokemonSlice.tsx
+++ Pokedex/src/features/pokemon/pokemonSlice.tsx
@@ -2,4 +2,35 @@ import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 
 // Aqui vai ser guardada a informação do pokemon
 
-import { Pokemon} from '../../models/Pokemon'
+import  Pokemon  from '../../models/Pokemon'
+
+const initialState: Pokemon = {
+    id:1,
+    name:'',
+    image:'',
+    height:0,
+    weigth:0,
+    type:'',
+    move:'',
+    stats:{
+    hp:0,
+    attack:0,
+    defense:0,
+    specialAttack:0,
+    specialDefense: 0,
+    speed: 0,
+    }
+}
+
+const pokemonSlice = createSlice({
+    name: 'pokemon',
+    initialState,
+    reducers:{
+        setPokemon: (state, action: PayloadAction<Pokemon>)=>{
+            state = action.payload;
+        }
+    }
+});
+
+export const {setPokemon} = pokemonSlice.actions;
+export default pokemonSlice.reducer;
